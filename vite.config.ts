8ceccf19afe9bb12c20f6d2ffbfe3e28b3edf6a8
ferret <1219460281@qq.com>
2025-04-24 15:34:31 +0800

    更新 Vite 配置，移除库构建设置并修改输出目录为 './docs'。

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,6 @@ import dts from 'vite-plugin-dts'
 export default defineConfig({
   plugins: [react(), tailwindcss(), dts({ rollupTypes: true, include: ['./src/components/SwipeMenu'], tsconfigPath: './tsconfig.app.json' })],
   build: {
-    lib: {
-      entry: './src/components/SwipeMenu/index.ts',
-      name: "SwipeMenuReact",
-      fileName: format => `index.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "react",
-          "react-dom": "react-dom",
-        },
-      },
-
-    },
+    outDir: './docs'
   },
 })
