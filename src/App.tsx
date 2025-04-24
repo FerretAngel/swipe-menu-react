410c5f940e4f350427d6002caa9d57626444d554
ferret <1219460281@qq.com>
2025-04-24 17:05:24 +0800

    重构 App 组件，使用循环渲染多个 SwipeMenu 实例，并调整布局样式以增强可读性。

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,28 @@ import SwipeMenu from "swipe-menu-react";
 import { Icon } from "@iconify/react";
 function App() {
   return (
-    <main className="w-[100dvw] h-[100dvh] grid place-items-center">
-      <SwipeMenu
-        className="w-md max-w-full gap-1 p-1"
-        asideClassName="flex items-center gap-1"
-        maxWidth="6rem"
-        aside={
-          <>
-            <button className="text-blue-500 text-2xl">
-              <Icon icon="lucide:edit" />
-            </button>
-            <button className="text-red-500 text-2xl">
-              <Icon icon="lucide:trash-2" />
-            </button>
-          </>
-        }>
-        <button className="truncate w-full h-full text-start">
-          A Simple Description to test the swipe menu
-        </button>
-      </SwipeMenu>
+    <main className="w-[100dvw] h-[100dvh] flex flex-col gap-1 justify-center items-center">
+      {new Array(4).fill(0).map((_, index) => (
+        <SwipeMenu
+          key={index}
+          className="w-md max-w-full gap-1 p-1"
+          asideClassName="flex items-center gap-1"
+          maxWidth="6rem"
+          aside={
+            <>
+              <button className="text-blue-500 text-2xl">
+                <Icon icon="lucide:edit" />
+              </button>
+              <button className="text-red-500 text-2xl">
+                <Icon icon="lucide:trash-2" />
+              </button>
+            </>
+          }>
+          <button className="truncate w-full h-full text-start">
+            A simple description to test the swipe menu.
+          </button>
+        </SwipeMenu>
+      ))}
     </main>
   );
 }
